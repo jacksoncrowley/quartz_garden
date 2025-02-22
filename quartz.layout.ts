fdcc3a73cb5a900b31dc3fcbf4cece1e66baf7a9
jackson <jacksonlcrowley@gmail.com>
2025-02-22 10:15:36 +0100

    Quartz sync: Feb 22, 2025, 10:15 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,10 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.PageTitle(),
+    Component.LinksHeader(),
+  ],
   afterBody: [],
    footer: Component.Footer({
      links: {
@@ -23,7 +26,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     //Component.Search(),
     //Component.Darkmode(),
@@ -40,7 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     //Component.Search(),
     //Component.Darkmode(),
---
+++ quartz/components/LinksHeader.tsx
@@ -0,0 +1,30 @@
+import { QuartzComponentConstructor } from "./types"
+import style from "./styles/linksHeader.scss"
+
+interface Options {
+  links: Record<string, string>
+}
+
+export default (() => {
+  function LinksHeader() {
+    return (
+      <div>
+        <div id="links-header">
+          <span>
+            <a href="Blog/">Blog</a>
+          </span>
+          <span>
+            <a href="/Protocols/">Protocols</a>
+          </span>
+          <span>
+            <a href="/CV/">CV</a>
+          </span>
+        </div>
+        <hr />
+      </div>
+    )
+  }
+
+  LinksHeader.css = style
+  return LinksHeader
+}) satisfies QuartzComponentConstructor
